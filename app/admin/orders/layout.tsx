0e16624ccf8aa382e6b46b55091e74e03089cdae
Tu Ho <thanhtu10c9@gmail.com>
2026-06-20 17:42:18 -0700

    refactor: simplify AdminOrderModalPage and AdminOrdersLayout components by removing unused code

--- app/admin/orders/layout.tsx
+++ app/admin/orders/layout.tsx
@@ -1,14 +1,7 @@
 export default function AdminOrdersLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
 }) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  );
+  return children;
 }
